a5c5d4a0b4d1124e6bdc985cadd1c1e4db1d7d65
Panca Aji Nugraha <78069598+panji665@users.noreply.github.com>
2021-11-02 16:44:35 +0700

    Add files via upload

--- Tugas PHP-PROJECT 25-30/kategori/update.php
+++ Tugas PHP-PROJECT 25-30/kategori/update.php
@@ -1,10 +1,23 @@
-<h3>Insert Kategori</h3>
+<?php
 
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+    $sql = "SELECT * FROM tblkategori WHERE idkategori=$id";
+
+    $row = $db->getItem($sql);
+}
+
+?>
+
+
+
+<h3>Update Kategori</h3>
 <div>
     <form action="" method="post">
         <div class="mb-3 w-50">
             <label for="">Nama Kategori</label>
-            <input type="text" name="kategori" required placeholder="isi kategori" class="form-control">
+            <input type="text" name="kategori" required value="<?php echo $row['kategori'] ?>" class="form-control">
         </div>
 
         <div>
@@ -18,11 +31,10 @@
 if (isset($_POST['simpan'])) {
     $kategori = $_POST['kategori'];
 
-    $sql = "INSERT INTO tblkategori VALUES ('','$kategori')";
+    $sql = "UPDATE tblkategori SET kategori =  '$kategori' WHERE idkategori = $id";
 
     $db->runSQL($sql);
 
     header("location:?f=kategori&m=select");
 }
-
 ?>
